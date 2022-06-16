ea56aa646ee120488e73ac330b89591980c4148f
aravind <aravindhraj283@gmail.com>
2022-06-16 21:25:14 +0530

    Columns & Rows

--- app/src/main/java/com/aravind/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/aravind/jetpackcompose/MainActivity.kt
@@ -3,12 +3,18 @@ package com.aravind.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.aravind.jetpackcompose.ui.theme.JetPackComposeTheme
 
@@ -16,26 +22,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+//
+//      <--- -   Column basic details ---->
+//            Column(
+//                Modifier
+//                    .fillMaxSize(0.5f)
+//                    .background(Color.Blue),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                verticalArrangement = Arrangement.Center
+//            ) {
+//                Text(text = "Aravind")
+//                Text(text = "Kumar")
+//
+//            }
+
+            Row (
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Blue),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                    ){
+                Text(text = "Aravind")
+                Text(text = "Kumar")
+                Text(text = "Raj")
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetPackComposeTheme {
-        Greeting("Android")
+           }
     }
 }

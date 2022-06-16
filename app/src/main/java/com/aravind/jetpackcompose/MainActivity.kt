51ee8d9c3bbfeee895e5f389815f17badeb49310
aravind <aravindhraj283@gmail.com>
2022-06-16 21:57:59 +0530

    Modifiers

--- app/src/main/java/com/aravind/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/aravind/jetpackcompose/MainActivity.kt
@@ -16,26 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetPackComposeTheme {
-        Greeting("Android")
     }
 }

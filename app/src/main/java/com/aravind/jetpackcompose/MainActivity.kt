96602bb9da088e01a03ac624f2cf7b00731132cd
aravind <aravindhraj283@gmail.com>
2022-06-17 16:52:18 +0530

    TextField

--- app/src/main/java/com/aravind/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/aravind/jetpackcompose/MainActivity.kt
@@ -3,20 +3,57 @@ package com.aravind.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.aravind.jetpackcompose.ui.theme.JetPackComposeTheme
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
+            // Provides default material styles(Toolbar,drawer)
+            val scaffoledState = rememberScaffoldState()
+            var textFiledState by remember{
+                mutableStateOf("")
+            }
+            val scope = rememberCoroutineScope()
+
+            Scaffold(scaffoldState = scaffoledState) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp)
+                    ) {
+                    TextField(
+                        value = textFiledState,
+                        label = {
+                            Text(text = "Enter Name")
+                        },
+                        onValueChange = {
+                            textFiledState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Button(onClick = {
+                        //Not good directly calling coroutine
+                        scope.launch {
+                            scaffoledState.snackbarHostState.showSnackbar("Hello $textFiledState")
+
+                        }
+                    }) {
+                        Text(text = "Click Me")
+                    }
+                }
+            }
         }
     }
 }
